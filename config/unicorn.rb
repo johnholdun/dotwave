d0b4320cc46019cb0fbe4ae0ad695b889f8c4381
John Holdun <john@johnholdun.com>
2016-01-29 03:05:17 -0500

    more config of course

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,13 +1,13 @@
-dir = File.expand_path('../../..', __FILE__)
+dir = File.expand_path('../..', __FILE__)
 
 worker_processes 3
 working_directory dir
 
 timeout 30
 
-listen "#{dir}tmp/sockets/unicorn.sock", backlog: 64
+listen "#{dir}/tmp/sockets/unicorn.sock", backlog: 64
 
-pid "#{dir}tmp/pids/unicorn.pid"
+pid "#{dir}/tmp/pids/unicorn.pid"
 
-stderr_path "#{dir}log/unicorn.stderr.log"
-stdout_path "#{dir}log/unicorn.stdout.log"
+stderr_path "#{dir}/log/unicorn.stderr.log"
+stdout_path "#{dir}/log/unicorn.stdout.log"
