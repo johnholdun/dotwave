4875f7537cdfd3229641e20d7543feb82f2f9a1b
John Holdun <john@johnholdun.com>
2016-02-12 00:25:57 -0500

    Alternatively, write new data to a CSV for copying to a remote database

---
+++ update-csv.rb
@@ -0,0 +1,59 @@
+require 'rubygems'
+require 'bundler'
+require 'csv'
+
+Bundler.require
+Dotenv.load
+
+DB = Sequel.connect ENV['DATABASE_URL']
+
+Dir.glob('lib/*.rb').map { |f| require_relative f }
+
+RSpotify.authenticate ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET']
+
+puts 'Fetching data...'
+
+updater = Updater.new(RSpotify).tap(&:update!)
+
+puts 'Comparing existing data...'
+
+existing_ids =
+  Hash[
+    [
+      [Album, updater.albums],
+      [Artist, updater.artists]
+    ].map do |klass, records|
+      ids = klass.model.select(:id).where(id: records.map(&:id)).map :id
+      [klass, ids]
+    end
+  ]
+
+puts 'Building rows...'
+
+tables = {
+  albums: [%i(id name release_date type)],
+  artists: [%i(id name popularity)],
+  album_artists: [%i(album_id artist_id)]
+}
+
+table_classes = {
+  Album => :albums,
+  Artist => :artists
+}
+
+(updater.artists.all + updater.albums).each do |record|
+  existing = existing_ids[record.class].include? record.id
+  next if existing
+  header = tables[table_classes[record.class]].first
+  tables[table_classes[record.class]] << header.map { |k| record.send k }
+  next unless record.is_a?(Album)
+  record.artists.each do |artist|
+    tables[:album_artists] << [record.id, artist.id]
+  end
+end
+
+puts 'Writing tables...'
+
+tables.each do |name, rows|
+  File.write "#{name}.csv", rows.map(&:to_csv).join
+end
