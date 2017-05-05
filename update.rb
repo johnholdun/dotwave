0c14da41fe6f7fb8303e748218f436ada6ccd981
John Holdun <john@johnholdun.com>
2017-05-05 03:04:12 -0400

    New experimental in-database updater

--- update.rb
+++ update.rb
@@ -8,29 +8,9 @@ DB = Sequel.connect ENV['DATABASE_URL']
 
 Dir.glob('lib/*.rb').map { |f| require_relative f }
 
-RSpotify.authenticate ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET']
+step = ARGV.first
 
-updater = Updater.new(RSpotify).tap(&:update!)
+puts 'Fetching data...'
 
-DB.transaction do
-  existing_ids =
-    Hash[
-      [
-        [Album, updater.albums],
-        [Artist, updater.artists]
-      ].map do |klass, records|
-        ids = klass.model.select(:id).where(id: records.map(&:id)).map :id
-        [klass, ids]
-      end
-    ]
-
-  (updater.artists.all + updater.albums).each do |record|
-    existing = existing_ids[record.class].include? record.id
-    method = existing ? :update : :insert
-    record.class.model.where(id: record.id).send method, record.as_json
-    next unless record.is_a?(Album) && !existing
-    record.artists.each do |artist|
-      DB[:album_artists].insert album_id: record.id, artist_id: artist.id
-    end
-  end
-end
+RSpotify.authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET'])
+UpdaterExperiment.call(RSpotify, DB, step)
--- update.sh
+++ update.sh
@@ -1,3 +1,1 @@
-ruby update-csv.rb \
-&& scp -i dotwave.pem *.csv ec2-user@dotwave.johnholdun.com:/var/www/dotwave/ \
-&& ssh -i dotwave.pem ec2-user@dotwave.johnholdun.com "cd /var/www/dotwave && ruby load-csv.rb"
+ssh -i dotwave.pem ec2-user@dotwave.johnholdun.com "cd /var/www/dotwave && ruby update.rb"
