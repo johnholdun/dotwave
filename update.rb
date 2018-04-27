9ace74ac91e9b957ddd0308546e51a039091893e
John Holdun <john@johnholdun.com>
2018-04-27 00:26:37 -0400

    Use absolute paths in updater

--- update.rb
+++ update.rb
@@ -1,13 +1,16 @@
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __FILE__)
+$:.unshift File.dirname(__FILE__)
+
 require 'rubygems'
 require 'bundler'
 require 'pry'
 
 Bundler.require
-Dotenv.load
+Dotenv.load(File.expand_path('../.env', __FILE__))
 
 DB = Sequel.connect(ENV['DATABASE_URL'])
 
-Dir.glob('lib/*.rb').map { |f| require_relative f }
+Dir.glob(File.expand_path('../lib/*.rb', __FILE__)).map { |f| require_relative f }
 
 RSpotify.authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET'])
 
